006ea63b497ef755275f816691bafee353111aae
Daeron <gustavo.wy@gmail.com>
2018-12-12 14:20:46 -0200

    primeiro commit

---
+++ src/casoDeTeste/PrimeiroCT.java
@@ -0,0 +1,22 @@
+package casoDeTeste;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PrimeiroCT {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Public\\tools\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		//Esta linha é resposavel por maximizar a tela
+		driver.manage().window().maximize();
+		//Esta linha é responsavel por redirecionar para o site do github
+		driver.get("https://github.com/login");
+		// Esta linha identifica o elemento que iremos realizar inserção, além de realizar a inserção também.
+		driver.findElement(By.id("login_field")).sendKeys("meuusuario");
+		driver.findElement(By.id("password")).sendKeys("minhasenha");
+		driver.findElement(By.name("commit")).click();
+	}
+}
